2cac94401099021b87d6cebb5d5841916588ad62
Palladium <psvrishabh@gmail.com>
2021-12-16 00:04:23 +0530

    code refactoring for menu

--- src/aoc_2018/mod.rs
+++ src/aoc_2018/mod.rs
@@ -1,2 +1,10 @@
-pub mod day1;
-pub mod day2;
+use bevy::prelude::AppBuilder;
+
+mod day1;
+mod day2;
+
+pub fn setup_app(mut app: AppBuilder) -> AppBuilder {
+    app = day1::setup(app);
+    app = day2::setup(app);
+    app
+}
